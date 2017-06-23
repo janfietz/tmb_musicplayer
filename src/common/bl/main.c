dc9a2ebd9828d9a3af3a4096b75021f7a5c73d0c
janfietz <janfietz@gmail.com>
2017-06-23 20:58:57 +0200

    bl_main: read filsystem for firmware file and write it to the mcu

--- src/common/bl/main.c
+++ src/common/bl/main.c
@@ -19,6 +19,7 @@
 
 static void halt_error(uint8_t code)
 {
+    ledOff(LED_READ);
     while (1)
     {
         ledBlink(LED_STATUS, OSAL_MS2ST(200), OSAL_MS2ST(200), code);
@@ -45,83 +46,70 @@ int main(void)
 
     boardStart();
 
-
     ledOn(LED_STATUS);
 
-    chThdSleepMilliseconds(500);
-    //chprintf(DEBUG_CANNEL, "\r\n\r\n----- ToddlerMusicbox BOOT -----\r\n");
-#if HAL_USE_SDC
     #define BLOCK_SIZE 8
 
-    /* Mount filesystem. */
-    //chprintf(DEBUG_CANNEL, "Mount filesystem ...");
-    FATFS fs;
-    FRESULT result = f_mount(&fs, "", 1);
-    if (result == FR_OK)
-    {
-        ledOn(LED_DECODE);
-        //chprintf(DEBUG_CANNEL, " OK\r\n");
-        /* Check for update file. */
-        FIL fwufile;
-        result = f_open(&fwufile, "firmware.bin", FA_READ | FA_OPEN_EXISTING);
-
+    if (sdcConnect(&SDCD1) == HAL_SUCCESS) {
+        /* Mount filesystem. */
+        FATFS fs;
+        FRESULT result = f_mount(&fs, "/mnt/", 1);
         if (result == FR_OK)
         {
-            /* Erase flash partition. */
-
-            wdgReset(&WDGD1);
-#if HAL_USE_NVM_PARTITION
-            nvmMassErase(PARTITION_FW);
-#endif /* HAL_USE_NVM_PARTITION */
-
-            /* Write new firmware to flash. */
-            const DWORD update_size = f_size(&fwufile);
-            const size_t n_blocks = update_size / BLOCK_SIZE +
-                    ((update_size % BLOCK_SIZE) ? 1 : 0);
-
-            //chprintf(DEBUG_CANNEL, "Load firmware file.");
-
-            for (size_t i = 0; i < n_blocks; ++i)
+            /* Check for update file. */
             {
-                ledToggle(LED_READ);
-                wdgReset(&WDGD1);
+                FIL fwufile;
+                result = f_open(&fwufile, "firmware.bin", FA_READ | FA_OPEN_EXISTING);
 
-                UINT this_block_size = 0;
-                uint8_t buffer[BLOCK_SIZE] =
-                        { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+                if (result == FR_OK)
+                {
+                    ledOn(LED_READ);
+                    /* Erase flash partition. */
 
-                result = f_read(&fwufile, buffer, BLOCK_SIZE, &this_block_size);
-                if (result != FR_OK)
-                    halt_error(1);
+                    wdgReset(&WDGD1);
+                    nvmMassErase(PARTITION_FW);
 
-#if HAL_USE_NVM_PARTITION
-                if (nvmWrite(PARTITION_FW, i * BLOCK_SIZE, BLOCK_SIZE, buffer) != HAL_SUCCESS)
-                    halt_error(2);
-#endif /* HAL_USE_NVM_PARTITION */
+                    /* Write new firmware to flash. */
+                    const DWORD update_size = f_size(&fwufile);
+                    const size_t n_blocks = update_size / BLOCK_SIZE +
+                            ((update_size % BLOCK_SIZE) ? 1 : 0);
+
+                    for (size_t i = 0; i < n_blocks; ++i)
+                    {
+                        wdgReset(&WDGD1);
+
+                        UINT this_block_size = 0;
+                        uint8_t buffer[BLOCK_SIZE] =
+                                { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+
+                        result = f_read(&fwufile, buffer, BLOCK_SIZE, &this_block_size);
+                        if (result != FR_OK)
+                            halt_error(1);
+
+                        if (nvmWrite(PARTITION_FW, i * BLOCK_SIZE, BLOCK_SIZE, buffer) != HAL_SUCCESS)
+                            halt_error(2);
+
+                    }
+
+                    result = f_close(&fwufile);
+                    if (result != FR_OK)
+                        halt_error(3);
+
+                    result = f_unlink("firmware.bin");
+                    if (result != FR_OK)
+                        halt_error(4);
+
+                    ledOff(LED_READ);
+                }
             }
 
-            result = f_close(&fwufile);
+            result = f_mount(NULL, "", 1);
             if (result != FR_OK)
-                halt_error(3);
-
-            result = f_unlink("firmware.bin");
-            if (result != FR_OK)
-                halt_error(4);
-
-            //chprintf(DEBUG_CANNEL, "finished.\r\n");
+                halt_error(5);
         }
-        else
-        {
-            //chprintf(DEBUG_CANNEL, " FAILED\r\n");
-        }
-
-        result = f_mount(NULL, "", 1);
-        if (result != FR_OK)
-            halt_error(5);
-
-        ledOff(LED_DECODE);
+        sdcDisconnect(&SDCD1);
     }
-#endif /* HAL_USE_SDC */
+
     ledOff(LED_STATUS);
 
     boardStop();
